3247f095925edfd6af593f0cc57611d60ecde3e0
Ivan Ermolyuk <ivan.ermolyuk@gmail.com>
2023-07-16 18:18:00 +0300

    Комит Задания 3

--- str_func.py
+++ str_func.py
@@ -2,3 +2,7 @@
 def to_uppercase(string):
     '''Вывод полученной строки в заглавных буквах'''
     return string.upper()
+
+def capitalize_words(string):
+    '''Возвращает полученную строку с заглавными первыми буквами'''
+    return ' '.join(word.capitalize() for word in string.split())
